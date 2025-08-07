7642dd770c9a1617b632068675225d64669b4d38
Bob <rfewedw2@gmail.com>
2025-08-07 21:03:59 +0300

    Added base func

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ Path_To_ENV = BASE_DIR / '.env'
 
 load_dotenv(Path_To_ENV)
 
-Path_To_Reso
+Path_To
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 TG_BOT_API_KEY = os.environ.get("TG_BOT_API_KEY")
